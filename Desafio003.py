8e07cb555fb12b177c52f6712ece170f0b800c7b
italopenha <italopenha77@outlook.com>
2024-03-05 19:45:26 -0300

    Desafio 003

---
+++ Desafio003.py
@@ -0,0 +1,8 @@
+# Este programa lê dois números inteiros e mostra a soma entre eles
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+s = n1 + n2
+
+print(f'A soma entre {n1} e {n2}, vale {s}')
