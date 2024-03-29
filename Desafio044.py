0d9c55c19e5095c8d9ce6d6b2c3cd7157a43c368
Ítalo Penha <italopenha77@outlook.com>
2024-03-29 20:34:46 -0300

    Adicionando exercícios

---
+++ Desafio044.py
@@ -0,0 +1,36 @@
+# Este programa calcula o valor a ser pago por um produto, considerando o seu preço normal
+# e condição de pagamento.
+
+precoInicial = float(input('Digite o preço do produto: '))
+print(31 * '-')
+print('Escolha a condição de pagamento:')
+print('Digite 1 para à vista no dinheiro/cheque')
+print('Digite 2 para à vista no cartão')
+print('Digite 3 para em até 2x no cartão')
+print('Digite 4 para até 3x ou mais no cartão')
+
+formaPagamento = input()
+print(31 * '-')
+
+if formaPagamento == '1':
+    precoFinal = precoInicial - precoInicial * 0.10
+    print(f'À vista no dinheiro ou cheque você terá que pagar R$ {precoFinal:.2f}.')
+elif formaPagamento == '2':
+    precoFinal = precoInicial - precoInicial * 0.05
+    print(f'À vista no cartão você terá que pagar R$ {precoFinal:.2f}.')
+elif formaPagamento == '3':
+    parcelas = precoInicial / 2
+    print(f'Você terá que pagar R$ {precoInicial:.2f} em duas parcelas de R$ {parcelas:.2f}.')
+elif formaPagamento == '4':
+    precoFinal = precoInicial + precoInicial * 0.20
+
+    qtdParcelas = 0
+    while qtdParcelas < 3:
+        qtdParcelas = int(input('Em quantas parcelas você quer pagar? Mínimo 3.'))
+        if qtdParcelas < 3:
+            print('Quantidade de parcelas deve ser 3 ou mais!')
+
+    parcelas = precoFinal / qtdParcelas
+    print(f'Você terá que pagar R$ {precoFinal:.2f} em {qtdParcelas} parcelas de R$ {parcelas:.2f}.')
+else:
+    print('Opção inválida.')
---
+++ Desafio045.py
@@ -0,0 +1,36 @@
+# Este programa é um jogo de Jokenpô
+import random
+import time
+
+minhaJogada = input('Digite: Pedra, Papel ou Tesoura ')
+
+print('Gerando jogada...')
+time.sleep(1.5)
+
+jogadaPC = random.randrange(1, 4)
+objetoPC = ''
+
+if jogadaPC == 1:
+    objetoPC = 'pedra'
+elif jogadaPC == 2:
+    objetoPC = 'papel'
+else:
+    objetoPC = 'tesoura'
+
+print(30 * '*')
+print(f'Escolha do computador: {objetoPC}')
+print(f'Sua escolha: {minhaJogada}')
+print(30 * '*')
+
+if minhaJogada.lower() == 'pedra' and objetoPC == 'papel':
+    print('O computador venceu!')
+elif minhaJogada.lower() == 'pedra' and objetoPC == 'tesoura':
+    print('Parabéns, você venceu!')
+elif minhaJogada.lower() == 'papel' and objetoPC == 'tesoura':
+    print('O computador venceu!')
+elif minhaJogada.lower() == 'papel' and objetoPC == 'pedra':
+    print('Parabéns, você venceu!')
+elif minhaJogada.lower() == 'tesoura' and objetoPC == 'pedra':
+    print('O computador venceu!')
+elif minhaJogada.lower() == 'tesoura' and objetoPC == 'papel':
+    print('Parabéns, você venceu!')
