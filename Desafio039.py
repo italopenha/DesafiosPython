05ae5ce6e6ca9f64644376e8fa68cb000a194300
Ítalo Penha <italopenha77@outlook.com>
2024-03-29 21:35:02 -0300

    Melhorando Desafio039

--- Desafio039.py
+++ Desafio039.py
@@ -3,13 +3,22 @@
 # ou se já passou da hora. Além do tempo que falta no caso de ainda não ter chegado a hora.
 import datetime
 
-anoDeNascimento = int(input('Digite o seu ano de nascimento: '))
+genero = input('Você é homem ou mulher? Digite H para homem ou M para mulher: ')
 
-idade = datetime.datetime.now().year - anoDeNascimento
+if genero.lower() == 'h':
+    anoDeNascimento = int(input('Digite o seu ano de nascimento: '))
 
-if idade > 18:
-    print(f'Você passou já passou da idade de alistamento, você deveria ter se alistado a {idade - 18} anos atrás.')
-elif idade < 18:
-    print(f'Faltam {18 - idade} anos para você chegar a idade de alistamento.')
+    anoAtual = datetime.datetime.now().year
+    idade =  anoAtual - anoDeNascimento
+
+    print(f'Quem nasceu em {anoDeNascimento} tem {idade} anos em {anoAtual}.')
+    if idade > 18:
+        print(f'Seu alistamento foi a {idade - 18} anos atrás, em {anoAtual - (idade - 18)}.')
+    elif idade < 18:
+        print(f'Faltam {18 - idade} anos para você chegar a idade de alistamento, será em {anoAtual + (18 - idade)}.')
+    else:
+        print(f'Você deve se alistar esse ano!')
+elif genero.lower() == 'm':
+    print('Mulheres não precisam se alistar no Brasil.')
 else:
-    print(f'Você deve se alistar esse ano!')
+    print('Opção inválida!')
